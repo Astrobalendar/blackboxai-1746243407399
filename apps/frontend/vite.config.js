35142a09fe9f4bd4ec249ce41225366a13430ba4
Coach Kura <coachkura@gmail.com>
2025-05-05 09:29:29 +0530

    Add base path to vite.config.js

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',  // Add this line
   root: 'public',  // Set root to public directory
   plugins: [vue()],
-  base: '/',
   define: {
     'process.env': {}
   },
