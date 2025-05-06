84a2b5ad10a42938984f5b5c0a7fdb544182705c
Astrobalendar <akuraastrology@gmail.com>
2025-05-06 10:00:27 +0530

    main.py

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+allow_origins = [
+    "https://akuraastrology.netlify.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
